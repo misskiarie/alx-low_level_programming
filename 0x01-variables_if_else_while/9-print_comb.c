4ed925a29f319d903dab3d054a5e1abb41e833fa
misskiarie <wambuikiarieofficial@gmail.com>
2023-02-20 22:01:01 +0300

    singles combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
 * main - prints all combos of single digits
+*
 * Return: Always 0
 */
 int main(void)
 {
 int n;
-for (n = 48; n < 58; n++)
-putchar(n);
-if (n != 57)
-{
+for (n = 48; n < 58; n++){
+putchar(n + '0');
+if (n != 57){
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
