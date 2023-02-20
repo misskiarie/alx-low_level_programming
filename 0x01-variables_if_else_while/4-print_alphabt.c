2fbea80f36120da30c4a1dd1cfeceb37b917d6d1
misskiarie <wambuikiarieofficial@gmail.com>
2023-02-20 21:09:16 +0300

    no e or q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,12 +6,12 @@
 */
 int main(void)
 {
-    char la;
-    for (la = 'a'; la <= 'z'; la++)
-    {
-        if (la != 'e' && la != 'q' )
-        putchar(la);
-    }
-    putchar('\n');
-    return (0);
+char la;
+for (la = 'a'; la <= 'z'; la++)
+{
+if (la != 'e' && la != 'q' )
+putchar(la);
+}
+putchar('\n');
+return (0);
 }
