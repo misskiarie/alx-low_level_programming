f0f7bf841c1c3699fc3522b830a90f9f91d19e7d
misskiarie <wambuikiarieofficial@gmail.com>
2023-02-20 22:05:34 +0300

    singles

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
-* main - prints all combos of single digits
+* main - prints single digits
 *
 * Return: Always 0
 */
 int main(void)
 {
 int n;
-for (n = 0; n < 100; n++)
+for (n = 0; n < 10; n++)
 {
 putchar(n + '0');
-if (n < 99)
+if (n < 9)
 {
 putchar(',');
 putchar(' ');
