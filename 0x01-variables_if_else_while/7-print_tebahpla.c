3ca2d8bd74c7e81da7100a8a09ae5c57f1837537
misskiarie <wambuikiarieofficial@gmail.com>
2023-02-20 21:28:37 +0300

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print reverse alphabets
+* Return: Always 0
+*/
+int main(void)
+{
+char le;
+for (le ='z'; le >= 'a'; le--)
+putchar(le);
+putchar('\n');
+return (0);
+}
