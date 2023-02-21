6350d5389088bbf00bed76173e1a353f973277e4
misskiarie <wambuikiarieofficial@gmail.com>
2023-02-21 17:13:46 +0300

    what is c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_isalpha - checks for alphabets in lower an upper case
+*@c: a charachter to be checked
+*Return: returns 0 or 1 depending on output
+*/
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') | (c >= 'A' && c <= 'B'));
+}
