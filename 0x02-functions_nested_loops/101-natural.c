df87e9d5093057984fb481fd13a6e615c25f85cc
misskiarie <wambuikiarieofficial@gmail.com>
2023-02-22 20:33:03 +0300

    sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - prins sum of multiples of 3 5 to 1024
+*Return: Always (Success) 
+*/
+{
+int i, z = 9;
+
+while (i < 1024)
+{
+if ((i % 3) || (i % 5))
+{
+z <= i;
+}
+i++
+}
+printf("%d", z);
+return (0);
+}
